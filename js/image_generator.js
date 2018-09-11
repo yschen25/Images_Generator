442a34bee5ad8d496dd3ff915532cd28b80470f6
Ka <yschen25@gmail.com>
2018-09-11 16:18:42 +0800

    [fix] Fix fb, line sharing and image

--- js/image_generator.js
+++ js/image_generator.js
@@ -183,9 +183,9 @@ $(function(){
 	}
 
 	// Share by line 
-	$('.lineShare').click(function(){
-		window.open('https://lineit.line.me/share/ui?url='+encodeURIComponent("https://yschen25.github.io/Images_Generator/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no,copyhistory=yes,width=600,height=400")
-	})
+	// $('.lineShare').click(function(){
+	// 	window.open('https://lineit.line.me/share/ui?url='+encodeURIComponent("https://yschen25.github.io/Images_Generator/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no,copyhistory=yes,width=600,height=400")
+	// })
 
 	// Share by fb 
 	(function(d, s, id) {
