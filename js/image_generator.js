aee60e7517fe90d2301a3db674abab8b9508b555
Ka <yschen25@gmail.com>
2018-09-11 17:54:02 +0800

    [fix] Fix fb sharing

--- js/image_generator.js
+++ js/image_generator.js
@@ -187,7 +187,7 @@ $(function(){
 		var js, fjs = d.getElementsByTagName(s)[0];
 		if (d.getElementById(id)) return;
 		js = d.createElement(s); js.id = id;
-		js.src = 'https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v3.1';
+		js.src = 'https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v3.1&appId=178599722664797&autoLogAppEvents=1';
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 
